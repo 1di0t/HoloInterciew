453aa7989683f5c9d267d409cce9d155379a695e
전성일 <jeonjerry0709@gmail.com>
2024-05-12 21:57:14 +0900

    Added a font and apllied it to movingPageButton

--- lib/widget/moving_page_button_widget.dart
+++ lib/widget/moving_page_button_widget.dart
@@ -5,29 +5,43 @@ class MovingPageButton extends StatelessWidget {
   final IconData iconData;
   final Widget nextPage;
   final bool isFocused;
+  final String buttonDetailTexts;
   const MovingPageButton({
     super.key,
     required this.iconData,
     required this.nextPage,
     required this.isFocused,
+    required this.buttonDetailTexts,
   });
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        padding: EdgeInsets.zero,
-        icon: Icon(
-          iconData,
-          size: 45,
-          color: setColor(isFocused),
-        ),
-        onPressed: () {
-          Navigator.pushReplacement(
-              context,
-              PageRouteBuilder(
-                  pageBuilder: (context, animation1, animation2) => nextPage,
-                  transitionDuration: Duration.zero,
-                  reverseTransitionDuration: Duration.zero));
-        });
+    return Column(
+      children: [
+        IconButton(
+            padding: EdgeInsets.zero,
+            icon: Icon(
+              iconData,
+              size: 45,
+              color: setColor(isFocused),
+            ),
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (context, animation1, animation2) =>
+                          nextPage,
+                      transitionDuration: Duration.zero,
+                      reverseTransitionDuration: Duration.zero));
+            }),
+        Text(
+          buttonDetailTexts,
+          style: const TextStyle(
+              fontFamily: 'NanumBarunpenB',
+              fontSize: 13,
+              color: Colors.black87),
+        )
+      ],
+    );
   }
 }
