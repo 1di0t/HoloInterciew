a613877b5dd9cf600cc32bd6d6af51c4890929de
전성일 <jeonjerry0709@gmail.com>
2024-06-10 12:39:26 +0900

    design undo

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:holo_interview/screens/login_page.dart';
-import 'package:holo_interview/widget/navigator_card_list_widget.dart';
+import '../screens/login_page.dart';
 import '../widget/bottom_navigator_bar_widget.dart';
+import '../constants.dart';
+import '../screens/interview_page.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> interviewSets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchInterviewSets();
+  }
+
+  Future<void> fetchInterviewSets() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('userId') ?? '';
+
+    if (userId.isNotEmpty) {
+      final response = await http.get(
+        Uri.parse(
+            '${Constants.flaskServer}/api/get_interview_sets?userId=$userId'),
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          interviewSets =
+              List<Map<String, dynamic>>.from(jsonDecode(response.body));
+        });
+      } else {
+        print('Failed to fetch interview sets: ${response.body}');
+      }
+    } else {
+      print('UserId is not set in SharedPreferences');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        //==================================================
-        //default padding in page
-        //==================================================
         padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            //==================================================
-            //SizedBox to aviod the camera
-            //==================================================
-            const SizedBox(
-              height: 60,
-            ),
-            //==================================================
-            //Title of the page
-            //==================================================
+            const SizedBox(height: 60),
             SizedBox(
               height: 60,
               child: Row(
@@ -36,37 +65,70 @@ class HomePage extends StatelessWidget {
                     "메인 페이지",
                     style: Theme.of(context).textTheme.displayLarge,
                   ),
-
-                  //==================================================
-                  //IconButton to navigate to the login page
-                  //==================================================
                   IconButton(
                     onPressed: () {
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginPage()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()),
+                      );
                     },
-                    icon: Image.asset("assets/images/holoInterview.png",
-                        width: const Size.fromWidth(60).width,
-                        height: const Size.fromHeight(60).height),
-                  )
+                    icon: Image.asset(
+                      "assets/images/holoInterview.png",
+                      width: const Size.fromWidth(60).width,
+                      height: const Size.fromHeight(60).height,
+                    ),
+                  ),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 20,
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: interviewSets.length,
+                itemBuilder: (context, index) {
+                  final interviewSet = interviewSets[index];
+                  return InkWell(
+                    focusColor: const Color(0x00000000),
+                    hoverColor: const Color(0x00000000),
+                    highlightColor: const Color(0x00000000),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InterViewPage(
+                              interviewSetId: interviewSet['interviewSetId']),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Theme.of(context).disabledColor,
+                      elevation: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '${interviewSet['company']} 면접 시작하기',
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            Image.asset(
+                              'assets/images/interview.png',
+                              width: 100,
+                              height: 100,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-            //==================================================
-            //NavigatorCard to navigate to the interview page
-            //==================================================
-            NavigatorCardList(isFeedback: false),
           ],
         ),
       ),
-      //==================================================
-      //BottomNavigationBar
-      //==================================================
       bottomNavigationBar: const BottomNavigatorBar(
         firstButton: false,
         secondButton: true,
